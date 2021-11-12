bff479df31f9a278aac4f53ac77ebcd2dc26c7d5
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-11 21:51:33 -0800

    trying

---
+++ _tests_/random.test.js
@@ -0,0 +1,7 @@
+
+const checkIfEqual = require('../lib/random.js')
+
+test('gets random number between 1 and 10', () =>{
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+})
