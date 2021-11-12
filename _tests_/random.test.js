da43122b6f5dd05d68dcd3d1b9bbea71c7f9f5f7
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-11 22:33:44 -0800

    wrote function for potion

--- _tests_/random.test.js
+++
@@ -1,7 +0,0 @@
-
-const checkIfEqual = require('../lib/random.js')
-
-test('gets random number between 1 and 10', () =>{
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-})
